4c3ad08bd6f15d6bf646ac1cde55235367eeb543
zineb baqloul <84290042+BAQLOULZINEB@users.noreply.github.com>
2025-05-18 15:51:22 +0100

    Initial commit

--- filtrage.php
+++ filtrage.php
@@ -3,7 +3,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "projects"; 
-
+//FONCTION pour donner a chaque produit son image 
 function findProductImage($productName) {
     $imageDir = 'images/prod_images/';
     $allowedExtensions = ['jpg', 'jpeg', 'png', 'gif', 'webp', 'avif'];
